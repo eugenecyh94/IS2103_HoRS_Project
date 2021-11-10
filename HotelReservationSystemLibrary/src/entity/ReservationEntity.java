b2489ce647afeb899b52f5e79faa202cbc59c750
Yunus <yunusali150201@gmail.com>
2021-11-11 02:19:07 +0800

    Added Sales Management Module Amended RoomRateSessionBean

--- HotelReservationSystemLibrary/src/entity/ReservationEntity.java
+++ HotelReservationSystemLibrary/src/entity/ReservationEntity.java
@@ -26,7 +26,7 @@ import javax.persistence.ManyToOne;
 public class ReservationEntity implements Serializable {
 
 //attributes
-     private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reservationId;
